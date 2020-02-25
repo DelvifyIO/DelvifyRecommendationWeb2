26e4f9a0fc7641683b64144b6a662979e9eccfd2
hooiyung <yung@delvify.io>
2020-02-25 16:11:03 +0800

    add image search

--- src/js/product.js
+++ src/js/product.js
@@ -59,9 +59,36 @@ const zips = ["SKU853410", "SKUA33400", "SKUA39265", "SKUA46233", "SKUA62285", "
 const long = ["SKU920791", "SKUB36075", "SKUB72460", "SKUB42173", "SKUB36442", "SKUB65105", "SKUB65106", "SKUB65107", "SKUB90023", "SKUB90034", "SKUB92225", "SKUC02371", "SKUB76918", "SKUB92214", "SKUB42159", "SKUB47872", "SKUB99443", "SKUB76989", "SKUB55194", "SKUC53026", "SKUB90418", "SKUC53068", "SKUC53372", "SKUC41156", "SKUC53391"];
 const dress = ["SKU895177","SKU898702","SKU904195","SKU908490","SKU908507","SKU911031","SKU911566","SKU920791","SKU920797","SKU922310","SKU922865","SKU945583","SKU972845","SKU986590","SKU986592","SKU993117","SKUA04028","SKUA06929","SKUA09638","SKUA07941","SKUA16542","SKUA18489","SKUA26304","SKUA27540","SKUA28349","SKUA42410","SKUA58280","SKUA55486","SKUA55487","SKU873031","SKU908047","SKU846369","SKUA51627","SKUA50320","SKUA45372","SKUA49778","SKUA36057","SKUA36369","SKUA55388","SKUA70803","SKUA69398","SKUA69881","SKUA62868","SKUA34918","SKUA70626","SKUA69367","SKUA69368","SKUA69399","SKUA68213","SKUA74760","SKUA74768","SKUA74275","SKUA71633","SKUA70589","SKUA72703","SKUA72704","SKUA73785","SKUA74151","SKUA68219","SKUA72705","SKUA73783","SKUA73784","SKUA73775","SKUA75073","SKUA75074","SKUA75784","SKUA75983","SKUA70599","SKUA70913","SKUA70973","SKUA71632","SKUA68144","SKUA68242","SKUB04630","SKUB04631","SKUB04632","SKUB10504","SKUB10540","SKUB10541","SKUB10555","SKUA59134","SKUA94311","SKUA94326","SKUA94343","SKUA94344","SKUA94348","SKUA97280","SKUA97655","SKUA97751","SKUA90983","SKUA92679","SKUA92684","SKUA92719","SKUA92725","SKUA81929","SKUA81930","SKUA81938","SKUA81947","SKUA81952","SKUA81953","SKUA81955","SKUA81956","SKUA83434","SKUA83454","SKUA83470","SKUA85111","SKUA85117","SKUA85118","SKUA85131","SKUA85145","SKUA85146","SKUA85155","SKUA85167","SKUA76761","SKUA76824","SKUA78659","SKUA79046","SKUB25153","SKUB25154","SKUB25170","SKUB25174","SKUB25175","SKUB25178","SKUB25179","SKUB25183","SKUB25185","SKUB25186","SKUB29879","SKUB36075","SKUB36433","SKUB72460","SKUB72516","SKUB77067","SKUB77068","SKUB25055","SKUB25135","SKUB25136","SKUB25140","SKUB25142","SKUB25145","SKUB65112","SKUB13846","SKUB13996","SKUA94301","SKUA95173","SKUA95178","SKUA95183","SKUA95212","SKUA95217","SKUA95223","SKUB56901","SKUB56909","SKUB56911","SKUA91496","SKUA91507","SKUA91524","SKUA91536","SKUA91543","SKUA91547","SKUA91550","SKUA91589","SKUA91605","SKUA91611","SKUA91650","SKUA92722","SKUB52303","SKUB52304","SKUB54000","SKUA85116","SKUB42110","SKUB42150","SKUB42154","SKUB42156","SKUB42168","SKUB42173","SKUB42186","SKUB40388","SKUB40389","SKUB40416","SKUB40417","SKUB40419","SKUB40439","SKUB40440","SKUB40456","SKUB36434","SKUB36442","SKUB36447","SKUB36500","SKUB36514","SKUB36625","SKU897807","SKUB13838","SKUB25051","SKUB25180","SKUB40387","SKUB52311","SKUB56469","SKUB56497","SKUB57565","SKUB57566","SKUB57567","SKUB65081","SKUB65087","SKUB65088","SKUB65105","SKUB65106","SKUB65107","SKUB76921","SKUB76925","SKUB76926","SKUB76940","SKUB76945","SKUB76947","SKUB76951","SKUB76956","SKUB76958","SKUB76959","SKUB83540","SKUB83561","SKUB90023","SKUB90029","SKUB90034","SKUB90041","SKUB90043","SKUB91774","SKUB92083","SKUB92106","SKUB92172","SKUB92225","SKUB92254","SKUB92583","SKUB92594","SKUB92600","SKUB92605","SKUB92607","SKUC02371","SKUC02409","SKUC02414","SKUC02415","SKUC02446","SKUC02510","SKUC03970","SKUB76917","SKUB76918","SKUB91755","SKUB92214","SKUB42159","SKUB83494","SKUB83495","SKUB42105","SKUB47973","SKUB83536",];
 
-const search = function(keyword, enabledAI, param) {
+const search = function(keyword, uploadedImage, enabledAI, param, searchBy) {
     return new Promise(function(resolve, reject) {
-        if (!enabledAI) {
+        if (searchBy === 'image') {
+            fetch(uploadedImage.data)
+                .then(res => res.blob())
+                .then(blob => {
+                    const file = new File([blob], uploadedImage.name);
+
+                    let formData = new FormData();
+                    formData.append('file', file);
+
+                    let req = new XMLHttpRequest();
+
+                    req.onreadystatechange = function(e) {
+                        if (req.readyState == 4 && req.status == 200) {
+                            const result = JSON.parse(req.responseText);
+                            const skus = result.skus.map(function (sku) {
+                                return sku.split('.')[0];
+                            });
+                            console.log('Image search', skus);
+                            api('GET', '/product', {skus: skus || [], ...param}, function (response) {
+                                resolve(response);
+                            });
+                        }
+                    };
+
+                    req.open("POST", 'http://13.67.88.182:5000/get_imageskus/');
+                    req.send(formData);
+                })
+        } else if (!enabledAI) {
             api('GET', '/product/search', { keyword: keyword, ...param }, function (response) {
                 console.log('Disabled AI', response.rows.map((item) => item.sku));
                 resolve(response);
@@ -85,19 +112,39 @@ $( document ).ready(function() {
     const page = getQuery()['p'] || 1;
     const categoryId = getQuery()['category'] || null;
     const keyword = getQuery()['keyword'] || null;
+    const searchBy = getQuery()['searchBy'] || null;
+    const uploadedImage = JSON.parse(sessionStorage.getItem('uploadedImage')) || null;
     const param = { limit, offset: (page - 1)* limit };
     const searchItems = {};
     if (categoryId) {
         param['categoryId'] = categoryId;
     }
-    if (keyword) {
-        $('#searchProduct').val(keyword);
-        $('#searchProductMobile').val(keyword);
-        search(keyword, enabledAI, param)
+    if (keyword || (searchBy === 'image' && uploadedImage)) {
+        if (searchBy === 'image' && uploadedImage) {
+            $('#uploadButton').removeClass('d-flex').addClass('d-none');
+            $('#searchButton').removeClass('d-flex').addClass('d-none');
+            $('#uploadSpinner').removeClass('d-none').addClass('d-flex');
+            $('#uploadedImageContainer').removeClass('d-none').addClass('d-flex');
+            $('#searchProduct').removeClass('d-block').addClass('d-none');
+            $('#uploadedImage').prop('src', uploadedImage.data);
+            $('#uploadedImageName').html(`${uploadedImage.name} ‧ ${(uploadedImage.size/1024).toFixed(2)}KB`);
+        } else {
+            $('#searchProduct').val(keyword);
+            $('#searchProductMobile').val(keyword);
+        }
+        search(keyword, uploadedImage, enabledAI, param, searchBy)
             .then((response) => {
+                $('#uploadButton').removeClass('d-none').addClass('d-flex');
+                $('#searchButton').removeClass('d-none').addClass('d-flex');
+                $('#uploadSpinner').removeClass('d-flex').addClass('d-none');
+                $('#searchProduct').removeClass('d-none').addClass('d-block');
                 const products = response.rows;
                 if (products.length <= 0) {
-                    $('#productList').append("<h3 class='w-100 text-center m-2'>No results for: '" + keyword + "'</h3>");
+                    if (searchBy === 'image' ) {
+                        $('#productList').append("<h3 class='w-100 text-center m-2'>No results for: '" + uploadedImage.name + "'</h3>");
+                    } else {
+                        $('#productList').append("<h3 class='w-100 text-center m-2'>No results for: '" + keyword + "'</h3>");
+                    }
                     return;
                 }
                 const pages = Math.ceil(response.count / limit);
--- src/js/searchBar.js
+++ src/js/searchBar.js
@@ -1,13 +1,6 @@
 $( document ).ready(function() {
     let enabledAI = getQuery()['ai'] === 'true' || false;
-    const submitForm = function(e) {
-        e.preventDefault();
-        const keyword = $('#searchProduct').val();
-        if (keyword) {
-            window.location.href = "product.html?keyword=" + keyword;
-        }
-        return false;
-    };
+    let loading = false;
 
     const toggleOn =
         '<button class="flex-c-m colorwhite color0-hov trans-0-4 float-right" id="toggleSearchButton">' +
@@ -36,8 +29,23 @@ $( document ).ready(function() {
     $('#searchBar').append(
         '<div class="search-product bo4 of-hidden bg-white">' +
         '<form id="searchForm" class="d-flex flex-row align-items-center">' +
-        '<input class="fs-25 size6 p-l-25 p-r-25" type="text" name="search-product" id="searchProduct" placeholder="Search Products...">' +
-        '<button class="flex-c-m size5 color2 color0-hov trans-0-4" id="searchButton">' +
+        '<div class="fs-25 size6 p-l-25 p-r-25 p-t-5 p-b-5 d-none align-items-center w-size-auto" id="uploadedImageContainer">' +
+        '<div class="p-1 h-100 d-flex align-items-center">' +
+        '<img class="h-100 mr-2" id="uploadedImage" style="max-width: 100px;"/>' +
+        '<span class="fs-15 w-size-max" id="uploadedImageName"></span>' +
+        '</div> ' +
+        '</div>' +
+        '<div class="fs-25 size6 p-l-25 p-r-25 d-block w-100 d-flex align-items-center">' +
+        '<input type="text" name="search-product" id="searchProduct" placeholder="Search Products..." />' +
+        '</div>' +
+        '<button class="flex-c-m size5 color2 color0-hov trans-0-4 d-flex" type="button" id="uploadButton">' +
+        '<i class="fs-20 fa fa-camera" aria-hidden="true" />' +
+        '</button>' +
+        '<div class="flex-c-m size5 color2 trans-0-4 d-none" id="uploadSpinner">' +
+        '<i class="fa fa-spinner fa-spin" aria-hidden="true" />' +
+        '</div>' +
+        '<input type="file" id="uploadInput" accept="image/*" hidden/>' +
+        '<button class="flex-c-m size5 color2 color0-hov trans-0-4" type="submit" id="searchButton">' +
         '<i class="fs-20 fa fa-search" aria-hidden="true" />' +
         '</button>' +
         '</form>' +
@@ -48,6 +56,19 @@ $( document ).ready(function() {
         '<div class="search-product of-hidden bg-white">' +
         '<form id="searchForm" class="d-flex flex-row align-items-center">' +
         '<input class="fs-15 size4 p-l-25 p-r-25" type="text" name="search-product" id="searchProduct" placeholder="Search Products...">' +
+        '<div class="fs-25 size6 p-l-25 p-r-25 p-t-5 p-b-5 d-none align-items-center" id="uploadedImageContainer">' +
+        '<div class="p-1 h-100 d-flex align-items-center">' +
+        '<img class="h-100 mr-2" id="uploadedImage" style="max-width: 50px;"/>' +
+        '<span class="fs-10" id="uploadedImageName"></span>' +
+        '</div> ' +
+        '</div>' +
+        '<button class="flex-c-m size8 color2 color0-hov trans-0-4 d-flex" type="button" id="uploadButton">' +
+        '<i class="fs-15 fa fa-camera" aria-hidden="true" />' +
+        '</button>' +
+        '<div class="flex-c-m size5 color2 trans-0-4 d-none" id="uploadSpinner">' +
+        '<i class="fa fa-spinner fa-spin" aria-hidden="true" />' +
+        '</div>' +
+        '<input type="file" id="uploadInput" accept="image/*" hidden/>' +
         '<button class="flex-c-m size8 color2 color0-hov trans-0-4" id="searchButton">' +
         '<i class="fs-15 fa fa-search" aria-hidden="true" />' +
         '</button>' +
@@ -58,9 +79,11 @@ $( document ).ready(function() {
     if (enabledAI) {
         $('#searchBar').append(toggleOn);
         $('#searchBarSmall').append(toggleOnSmall);
+        $('#uploadButton').removeClass('d-none').addClass('d-flex');
     } else {
         $('#searchBar').append(toggleOff);
         $('#searchBarSmall').append(toggleOffSmall);
+        $('#uploadButton').removeClass('d-flex').addClass('d-none');
     }
 
 
@@ -69,10 +92,14 @@ $( document ).ready(function() {
             enabledAI = false;
             $(this).find("i").removeClass("fa-toggle-on").addClass("fa-toggle-off");
             $(this).find("span").text("Enable Delvify AI Search");
+            $('#uploadButton').removeClass('d-flex').addClass('d-none');
         } else {
             enabledAI = true;
             $(this).find("i").removeClass("fa-toggle-off").addClass("fa-toggle-on");
             $(this).find("span").text("Disable Delvify AI Search");
+            if (!loading) {
+                $('#uploadButton').removeClass('d-none').addClass('d-flex');
+            }
         }
     });
 
@@ -80,10 +107,37 @@ $( document ).ready(function() {
         e.preventDefault();
         const keyword = $('#searchProduct').val();
         if (keyword) {
-            api('POST', `/query`, { query: keyword }, (result) => {
-                window.location.href = "product.html?" + "ai=" + enabledAI +"&keyword=" + keyword;
-            });
+            window.location.href = "product.html?" + "ai=" + enabledAI +"&keyword=" + keyword;
         }
         return false;
     });
+
+    $('#uploadButton').on('click', function(e) {
+        e.preventDefault();
+        $('#uploadInput').click();
+        return false;
+    });
+
+    $('#uploadInput').on('change', function(e) {
+        if ($(this).prop('files').length > 0) {
+            loading = true;
+            $('#uploadButton').removeClass('d-flex').addClass('d-none');
+            $('#searchButton').removeClass('d-flex').addClass('d-none');
+            $('#uploadSpinner').removeClass('d-none').addClass('d-flex');
+            $('#searchProduct').removeClass('d-block').addClass('d-none');
+            $('#uploadedImageContainer').removeClass('d-none').addClass('d-flex');
+            const file = $(this).prop('files')[0];
+            $('#uploadedImage').prop('src', URL.createObjectURL(file));
+            $('#uploadedImageName').html(`${file.name} ‧ ${(file.size/1024).toFixed(2)}KB`);
+
+
+            let reader = new FileReader();
+            reader.onloadend = function() {
+                sessionStorage.setItem('uploadedImage', JSON.stringify({ data: reader.result, name: file.name, size: file.size }));
+                window.location.href = "product.html?" + "ai=" + enabledAI +"&searchBy=image";
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
 });
